c9b91a89231e8fdf16a0c4e540eb57eee7af4a90
Scott-McKool <scott.lr.cool@gmail.com>
2022-11-22 21:17:34 -0500

    added install script

---
+++ install.sh
@@ -0,0 +1,49 @@
+#!/bin/sh
+
+# check for root (user id of 0)
+ [ `id -u` -ne 0 ] && echo "this script should be run as root" && exit 1
+
+# make a config file for inportant variables
+echo "creating config file 'justinConfig.py'"
+    cat > justinConfig.py <<CONFIGFILE
+PREFIX = "$"
+DISCORD_TOKEN = "$TOKEN"
+BOT_DIR = "$PWD/"
+CONFIGFILE
+
+### install requirements
+echo "installing pip with apt. . ."
+apt update && apt install python3-pip -y
+
+# install python packages
+echo "installing requirements with pip3. . ."
+pip3 install -r requirements.txt
+
+### use systemd to run stockbot on system startup
+echo "Setting up stockBot.py to run on system startup"
+# make a unit file for this systemd service
+echo "creating unit file 'stockBot.service'"
+    cat > stockBot.service <<UNITFILE
+[Unit]
+Description=Runs stockBot.py script on startup
+After=multi-user.target
+[Service]
+ExecStart=/usr/bin/python3 "$PWD/stockBot.py"
+[Install]
+WantedBy=multi-user.target
+UNITFILE
+# put the unitfile in its place w/ systemd
+mv stockBot.service /etc/systemd/system/stockBot.service
+# reload systemd so it can find this newly created service
+echo "reloding systemctl daemon"
+systemctl daemon-reload
+# enable this service in systemd
+echo "enabling stockBot.service"
+sudo systemctl enable stockBot.service
+echo "stockBot service has been added and enabled"
+echo "stockBot.py will be automatically run on startup from now on"
+echo ""
+echo "to dissable running on startup type 'sudo systemctl disable stockBot.service'"
+echo ""
+
+exit 0
